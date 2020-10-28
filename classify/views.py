0b08d5587664b0aeb92bd1c893fa03f395956342
Pritindra <37242669+pritindra@users.noreply.github.com>
2020-10-28 23:24:41 +0530

    Update views.py

--- classify/views.py
+++ classify/views.py
@@ -1,48 +1,68 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-import base64
-from django.core.files.base import ContentFile
-from django.core.files.storage import default_storage
-from django.conf import settings 
-from tensorflow.python.keras.backend import set_session
-from keras.preprocessing.image import load_img
-from keras.preprocessing.image import img_to_array
-from keras.applications.imagenet_utils import decode_predictions
-import matplotlib.pyplot as plt
-import numpy as np
-from keras.applications import vgg16
-import datetime
-import traceback
+# Create your views here.
+
+from django.core.files.storage import FileSystemStorage
+
+from keras.models import load_model
+from keras.preprocessing import image
+import tensorflow as tf
+import json
+from tensorflow import Graph, Session
 
 from django.views.generic import ListView, DetailView
 from .models import FruitDatabase
+from .filter import FruitFilter 
+
+img_height, img_width=224,224
+with open('./models/imagenet_classes.json','r') as f:
+    labelInfo=f.read()
+
+labelInfo=json.loads(labelInfo)
+
+
+model_graph = Graph()
+with model_graph.as_default():
+    tf_session = Session()
+    with tf_session.as_default():
+        model=load_model('./models/MobileNetModelImagenet.h5')
+
+
 
 def index(request):
-    if  request.method == "POST":
-        f=request.FILES['sentFile'] # here you get the files needed
-        response = {}
-        file_name = "pic.jpg"
-        file_name_2 = default_storage.save(file_name, f)
-        file_url = default_storage.url(file_name_2)
-        original = load_img(file_url, target_size=(224, 224))
-        numpy_image = img_to_array(original)
-        
+    context={'a':1}
+    return render(request,'index.html',context)
 
-        image_batch = np.expand_dims(numpy_image, axis=0)
-        # prepare the image for the VGG model
-        processed_image = vgg16.preprocess_input(image_batch.copy())
-        
-        # get the predicted probabilities for each class
-        with settings.GRAPH1.as_default():
-            set_session(settings.SESS)
-            predictions=settings.VGG_MODEL.predict(processed_image)
-       
-        label = decode_predictions(predictions)
-        label = list(label)[0]
-        response['name'] = str(label)
-        return render(request,'homepage.html',response)
-    else:
-        return render(request,'homepage.html')
+
+
+def predictImage(request):
+    print (request)
+    print (request.POST.dict())
+    fileObj=request.FILES['filePath']
+    fs=FileSystemStorage()
+    filePathName=fs.save(fileObj.name,fileObj)
+    filePathName=fs.url(filePathName)
+    testimage='.'+filePathName
+    img = image.load_img(testimage, target_size=(img_height, img_width))
+    x = image.img_to_array(img)
+    x=x/255
+    x=x.reshape(1,img_height, img_width,3)
+    with model_graph.as_default():
+        with tf_session.as_default():
+            predi=model.predict(x)
+
+    import numpy as np
+    global predictedLabel
+    predictedLabel=labelInfo[str(np.argmax(predi[0]))]
+    
+    context={'filePathName':filePathName,'predictedLabel':predictedLabel[1]}
+    return render(request,'index.html',context) 
+
+def viewDataBase(request):
+    import os
+    listOfImages=os.listdir('./media/')
+    listOfImagesPath=['./media/'+i for i in listOfImages]
+    context={'listOfImagesPath':listOfImagesPath}
+    return render(request,'viewDB.html',context) 
 
 class InfoView(DetailView):
     model = FruitDatabase
@@ -52,3 +72,9 @@ class InfoView(DetailView):
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
         return data
+
+
+def search(request):
+    search = FruitDatabase.objects.all()
+    fruit_filter = FruitFilter(request.GET, queryset=search)
+    return render(request, 'users/fruit_filter.html', {'filter': fruit_filter})
